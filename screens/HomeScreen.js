4304573a16fc90af8e4118fe34612b8d924c4c3f
graememick@gmail.com <graememick@gmail.com>
2022-08-11 22:05:39 +0900

    feat: fetch data from db

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,43 @@
 import { useNavigation } from '@react-navigation/core'
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Header, FlatList, ScrollView } from 'react-native'
 import { auth } from '../firebase'
+import axios from 'axios'
+import { ListItem } from 'react-native-elements'
+
 
 const HomeScreen = () => {
   const navigation = useNavigation()
+  const [recipes, setRecipes] = useState([{name: "tofu", description: "food"},{name: "rice", description: "food"}])
+
+  useEffect(() => {
+    axios
+      .get("https://seasonal-cookbook.herokuapp.com/api/recipes/Pot-au-feu")
+      .then((response) => {
+        setRecipes([response.data])
+      });
+  }, []);
+
+  // const displayRecipe = (recipesList) => {
+  //     return (
+  //       <FlatList
+  //       ItemSeparatorComponent={() => <View style={styles.separator} />}
+  //       data={recipesList}
+  //       keyExtractor={(item, index) => index.toString()}
+  //       renderItem={({item}) => {
+  //         return (
+  //           <ListItem
+  //           title={`${item.name}`}
+  //           subtitle={`${item.description}`}
+  //           onPress={() => { }}
+  //           />
+  //         );
+  //       }
+  //     }
+  //     />
+  //     );
+  //   }
+       
 
   const handleSignOut = () => {
     auth
@@ -17,13 +50,22 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Email: {auth.currentUser?.email}</Text>
+      <Text>recipes: {recipes[0].name}</Text>
+      <ScrollView>
+        {
+          recipes.map(item => (
+            // <View key={item.name}>
+              <text>{item?.name}</text>
+            // </View>
+          ))}
+      </ScrollView>
+      {/* <Text>Email: {auth.currentUser?.email}</Text>
       <TouchableOpacity
         onPress={handleSignOut}
         style={styles.button}
       >
         <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   )
 }
@@ -48,5 +90,12 @@ const styles = StyleSheet.create({
     color: '#be1e2d',
     fontWeight: '700',
     fontSize: 16,
+  },
+  heading: {
+
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "grey"
   },
 })
